e8bd98119b76451585a253b43974f3c149d0cdc7
Isra Habibi <isra.habibi@gmail.com>
2020-11-23 23:12:27 +0700

    nnn

--- KulkasSamsung.py
+++ KulkasSamsung.py
@@ -14,17 +14,20 @@ html_source = driver.page_source
 soup = BeautifulSoup(html_source, 'html.parser')
 
 dataSamsung = []
-shopGridd = soup.find('div', class_ = 'shop-grid')
-listKulkases = shopGridd.find_all('div', class_ = 'shop-grid-col shop-col3')
+shopGridd = soup.find('div', class_='shop-grid')
+listKulkases = shopGridd.find_all('div', class_='shop-grid-col shop-col3')
 for listKulkas in listKulkases:
-    tipeKulkas = listKulkas.find('span', class_ = 'cm-shop-card__serial').get_text()
-    namaKulkas = listKulkas.find('strong', class_ = 's-txt-title').get_text()
-    try :
-        hargaKulkas = listKulkas.find('span', class_ = 'cm-shop-card__price-num').get_text()
-    except :
+    tipeKulkas = listKulkas.find(
+        'span', class_='cm-shop-card__serial').get_text()
+    namaKulkas = listKulkas.find('strong', class_='s-txt-title').get_text()
+    try:
+        hargaKulkas = listKulkas.find(
+            'span', class_='cm-shop-card__price-num').get_text()
+    except:
         hargaKulkas = 0
-    dataSamsung.append((tipeKulkas,namaKulkas,hargaKulkas))
+    dataSamsung.append((tipeKulkas, namaKulkas, hargaKulkas))
 
-df = pd.DataFrame(dataSamsung,columns=['tipeKulkas','namaKulkas','hargaKulkas'])
+df = pd.DataFrame(dataSamsung, columns=[
+                  'tipeKulkas', 'namaKulkas', 'hargaKulkas'])
 
 df.to_csv('D:\\Project\\EveryDayCode\\TokPedScrapperOut\\'+'KulkasSamsung.csv')
